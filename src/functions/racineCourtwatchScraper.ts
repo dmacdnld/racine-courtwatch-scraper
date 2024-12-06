b24a1eec1dac156ec0812c3d18e07b38c88ff355
Daniel MacDonald <dmacdonald@fastmail.com>
2024-12-06 11:11:11 -0600

    Merge up from dev (#4)
    * add deploy action workflow (#1)
    
    * update timer trigger to account for UTC & DST (#3)

--- src/functions/racineCourtwatchScraper.ts
+++ src/functions/racineCourtwatchScraper.ts
@@ -1,10 +1,14 @@
 import { app, InvocationContext, Timer } from "@azure/functions";
 
-export async function racineCourtwatchScraper(myTimer: Timer, context: InvocationContext): Promise<void> {
-    context.log('Timer function processed request.');
+export async function racineCourtwatchScraper(
+  myTimer: Timer,
+  context: InvocationContext
+): Promise<void> {
+  context.log("Timer function processed request", JSON.stringify(myTimer));
 }
 
-app.timer('racineCourtwatchScraper', {
-    schedule: '0 30 12 * * 1-5',
-    handler: racineCourtwatchScraper
+app.timer("racineCourtwatchScraper", {
+  // Run Mon-Fri @ 11:30 am & 12:30 pm CT to account for DST
+  schedule: "0 30 17-18 * * 1-5",
+  handler: racineCourtwatchScraper,
 });
