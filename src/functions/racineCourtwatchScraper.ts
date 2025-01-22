4576171c39693e937797e97490c08cf191c44730
Daniel MacDonald <danielmacdonald@MacBookAir.lan>
2025-01-21 22:16:33 -0600

    use http trigger

--- src/functions/racineCourtwatchScraper.ts
+++ src/functions/racineCourtwatchScraper.ts
@@ -1,4 +1,10 @@
-import { app, InvocationContext, Timer } from "@azure/functions";
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+  Timer,
+} from "@azure/functions";
 
 import { scrape } from "../utils/scrape";
 
@@ -11,8 +17,24 @@ export async function racineCourtwatchScraper(
   context.log("Timer function processed request");
 }
 
-app.timer("racineCourtwatchScraper", {
-  // Run Mon-Fri @ 11:30 am & 12:30 pm CT to account for DST
-  schedule: "* */1 * * * *",
-  handler: racineCourtwatchScraper,
+// app.timer("racineCourtwatchScraper", {
+//   // Run Mon-Fri @ 11:30 am & 12:30 pm CT to account for DST
+//   schedule: "* */10 * * * *",
+//   handler: racineCourtwatchScraper,
+// });
+
+export async function httpTrigger1(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log("HTTP function processing request");
+  await scrape(context);
+  context.log("HTTP function processed request");
+  return { status: 200 };
+}
+
+app.http("httpTrigger1", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: httpTrigger1,
 });
